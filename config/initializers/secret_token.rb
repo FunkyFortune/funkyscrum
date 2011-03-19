c30a511bc685cbaba10b4fbb72687dadd10f3bd6
eianco <eiancoun@gmail.com>
2011-03-19 17:07:23 -0400

    initial checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Funkyscrum::Application.config.secret_token = '218c9cd03559dd4f763e84068574ddaa1c7c789e94382782b6fbea40a013e61e5067ed10fcd92f1336f1e30b7486e333fbd52ea08efcb7a12ec39fb20e25a291'
