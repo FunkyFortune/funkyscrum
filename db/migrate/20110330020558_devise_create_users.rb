83cda21ac89fbd706776a0b8f0ead8e272f57d02
eian <eiancoun@gmail.com>
2011-04-04 22:48:42 -0400

    fix migrations so that we don't depend on Devise just to migrate

--- db/migrate/20110330020558_devise_create_users.rb
+++ db/migrate/20110330020558_devise_create_users.rb
@@ -1,10 +1,10 @@
 class DeviseCreateUsers < ActiveRecord::Migration
   def self.up
     create_table(:users) do |t|
-      t.database_authenticatable :null => false
-      t.recoverable
-      t.rememberable
-      t.trackable
+      #t.database_authenticatable :null => false
+      #t.recoverable
+      #t.rememberable
+      #t.trackable
 
       # t.encryptable
       # t.confirmable
@@ -15,8 +15,21 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.timestamps
     end
 
-    add_index :users, :email,                :unique => true
-    add_index :users, :reset_password_token, :unique => true
+    # stick in a bunch of micky mouse columns so that when we remove them later on 
+    # migrations won't puke
+    
+    add_column :users, :email, :string
+    add_column :users, :encrypted_password, :string
+    add_column :users, :reset_password_token, :string
+    add_column :users, :remember_created_at, :datetime
+    add_column :users, :sign_in_count, :integer
+    add_column :users, :current_sign_in_at, :datetime
+    add_column :users, :last_sign_in_at, :datetime
+    add_column :users, :current_sign_in_ip, :string
+    add_column :users, :last_sign_in_ip, :string
+
+    #add_index :users, :email,                :unique => true
+    #add_index :users, :reset_password_token, :unique => true
     # add_index :users, :confirmation_token,   :unique => true
     # add_index :users, :unlock_token,         :unique => true
     # add_index :users, :authentication_token, :unique => true
